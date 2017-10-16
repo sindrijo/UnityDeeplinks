bde1b3e6ecbe37546cc54965595ea4628e508595
Sindri Jóelsson <sindrijo@gmail.com>
2017-10-16 15:25:29 +0200

    Pass generated json as argument to script

--- Assets/Plugins/UnityDeeplinks/Android/Editor/DeeplinksBuildPathHelper.cs
+++ Assets/Plugins/UnityDeeplinks/Android/Editor/DeeplinksBuildPathHelper.cs
@@ -1,18 +1,76 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public static class DeeplinksBuildPathHelper 
 {
     [InitializeOnLoadMethod]
     private static void Init()
     {
+        CreateHelperFile();
+    }
+
+    private static string GetScriptName()
+    {
+        return typeof(DeeplinksBuildPathHelper).Name + ".cs";
+    }
+
+    private static string GetContainingDirectoryPath()
+    {
+        var scriptPath = Directory.GetFiles(Application.dataPath, GetScriptName(), SearchOption.AllDirectories).First();
+        return scriptPath.Replace(GetScriptName(), "");
+    }
+
+    [MenuItem("Tools/Deeplinks/Build Android Plugin")]
+    private static void BuildAndroidPluginFromScript()
+    {
+        CreateHelperFile();
+
+        var parentPath = Directory.GetParent(GetContainingDirectoryPath().Replace("/", @"\")).Parent;
+        if (parentPath == null)
+        {
+            Debug.LogError("Path error.");
+            return;
+        }
+
+        var rawJson = EditorJsonUtility.ToJson(ConstructJarConfig(), false);
+        var escapedJson = rawJson.Replace(@"\""", @"\\\""") // escape [\"] at end of string with [\\\"]
+                                 .Replace(@"""", @"\""");  // escape ["] in json with [\"]
+
+        var quoteWrappedEscapedJson = string.Format(@"""{0}""", escapedJson);
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = "powershell.exe",
+            Arguments = string.Format(@"& '{0}' '{1}'", parentPath.FullName + "\\" + "build_jar.ps1", quoteWrappedEscapedJson),
+            WorkingDirectory = parentPath.FullName,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+        };
+
+        using (var process = new Process { StartInfo = psi })
+        {
+            process.Start();
+            string standardOutput;
+            while ((standardOutput = process.StandardOutput.ReadLine()) != null)
+            {
+                Debug.Log(standardOutput);
+            }
+            process.WaitForExit();
+        }
+    }
+
+
+
+    private static void CreateHelperFile()
+    {
         const string FileName = "deeplink-build-conf.json";
-        string scriptName = typeof(DeeplinksBuildPathHelper).Name + ".cs";
-        var scriptPath = Directory.GetFiles(Application.dataPath, scriptName, SearchOption.AllDirectories).First();
-        var pathsJsonPath = Path.Combine(scriptPath.Replace('/', '\\').Replace(scriptName, ""), "Resources\\" + FileName);
+        var pathsJsonPath = Path.Combine(GetContainingDirectoryPath(), "Resources\\" + FileName);
 
         if (File.Exists(pathsJsonPath))
         {
@@ -25,7 +83,16 @@ public static class DeeplinksBuildPathHelper
             return;
         }
 
-        var p = new Paths
+        var json = EditorJsonUtility.ToJson(ConstructJarConfig(), true);
+        Debug.Log(FileName + " : " + json);
+
+        Debug.LogWarning("Writing to : " + pathsJsonPath);
+        File.WriteAllText(pathsJsonPath, json);
+    }
+
+    private static JarBuildConfig ConstructJarConfig()
+    {
+        var p = new JarBuildConfig
         {
             UnityProjectRootPath = Directory.GetParent(Application.dataPath).FullName,
             UnityEditorDataPath = EditorApplication.applicationContentsPath.Replace('/', '\\'),
@@ -34,15 +101,11 @@ public static class DeeplinksBuildPathHelper
             AndroidPackageName = new string(PlayerSettings.applicationIdentifier.Reverse().SkipWhile(c => c != '.').Skip(1).Reverse().ToArray())
         };
 
-        var json = EditorJsonUtility.ToJson(p, true);
-        Debug.Log(FileName + " : " + json);
-
-        Debug.LogWarning("Writing to : " + pathsJsonPath);
-        File.WriteAllText(pathsJsonPath, json);
+        return p;
     }
 
     [Serializable]
-    private class Paths
+    private class JarBuildConfig
     {
         public string UnityProjectRootPath;
 
