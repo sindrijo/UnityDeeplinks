d5dcaca1c4b64df0bb4be6e97fc93ed074492039
Sindri Jóelsson <sindrijo@gmail.com>
2018-05-16 12:13:26 +0200

    Only detect and use latest version if android api level is 0 (auto)
    Also added more logging

--- Assets/Plugins/UnityDeeplinks/Android/build_jar.ps1
+++ Assets/Plugins/UnityDeeplinks/Android/build_jar.ps1
@@ -1,3 +1,5 @@
+ param ([string]$jsonArg )
+
 ### functions
 function CreateDirectoryIfNeeded($Path, $m) {
     if(-Not(Test-Path $Path)) {
@@ -5,36 +7,60 @@ function CreateDirectoryIfNeeded($Path, $m) {
     }
 }
 ### /end-functions
+Write-Output "JSON-----"
 
-$jsonObject = Get-Content $pwd'\Editor\Resources\deeplink-build-conf.json' | ConvertFrom-Json
+if($jsonArg)
+{
+    Write-Output "Using json arg: $jsonArg"
+    $buildConfig = ConvertFrom-Json $jsonArg
+}
+else {
+    Write-Output "Reading json from file"
+    $buildConfig = Get-Content $pwd'\Editor\Resources\deeplink-build-conf.json' | ConvertFrom-Json    
+}
 
-$env:UNITY_LIBS = $jsonObject.UnityEditorDataPath + '\PlaybackEngines\AndroidPlayer\Variations\mono\Release\Classes\classes.jar'
 
+$env:UNITY_LIBS = $buildConfig.UnityEditorDataPath + '\PlaybackEngines\AndroidPlayer\Variations\mono\Release\Classes\classes.jar'
 if(-Not(Test-Path $env:UNITY_LIBS)) {
     throw "Unity libs not found @ $env:UNITY_LIBS"
 }
 
-$env:JDK_HOME = $jsonObject.JdkPath
+$env:JDK_HOME = $buildConfig.JdkPath
 if(-Not(Test-Path $env:JDK_HOME)) {
     throw "JDK not found @ $env:JDK_HOME"
 }
 
-$env:ANDROID_SDK_ROOT = $jsonObject.AndroidSdkRoot
+$env:ANDROID_SDK_ROOT = $buildConfig.AndroidSdkRoot
 if(-Not(Test-Path $env:ANDROID_SDK_ROOT)) {
     throw "Android SDK not found @ $env:ANDROID_SDK_ROOT"
 }
 
-#determine versions to use, descending sort should give us the latest version
-$latestAndroidVersionFolderName = (Get-ChildItem -Path ($env:ANDROID_SDK_ROOT + "\platforms\") -Directory |
-     Where-Object {$_.Name -Match 'android-([0-9]{2,})$'} | 
-     Sort-Object -Descending |
-     Select-Object -First 1).Name
+$targetAndroidApiLevel = $buildConfig.AndroidTargetSdkVersion
+$env:ANDROID_PLATFORM_JAR = "$env:ANDROID_SDK_ROOT\platforms\android-$targetAndroidApiLevel\android.jar"
 
-$env:ANDROID_PLATFORM_JAR= "$env:ANDROID_SDK_ROOT\platforms\$latestAndroidVersionFolderName\android.jar"
-$env:BOOTCLASSPATH= $env:JDK_HOME + "jre\lib"
-$env:CLASSPATH= "$env:UNITY_LIBS;$env:ANDROID_PLATFORM_JAR"
+if($targetAndroidApiLevel -eq 0){
 
-$unityProjectRootPath = $jsonObject.UnityProjectRootPath
+    Write-Output "Target Android Api-Level is $targetAndroidApiLevel, using latest Android SDK"
+
+    #determine latest version available, descending sort should give us the latest version
+    $latestAndroidApiLevelFolderName = (Get-ChildItem -Path ($env:ANDROID_SDK_ROOT + "\platforms\") -Directory |
+       Where-Object {$_.Name -Match 'android-([0-9]{2,})$'} | 
+       Sort-Object -Descending |
+       Select-Object -First 1).Name
+
+    $env:ANDROID_PLATFORM_JAR = "$env:ANDROID_SDK_ROOT\platforms\$latestAndroidApiLevelFolderName\android.jar"
+}
+
+if(-Not(Test-Path $env:ANDROID_PLATFORM_JAR)) {
+    throw "Target Android SDK not found @ $env:ANDROID_PLATFORM_JAR"
+} else {
+    Write-Output "Using Android SDK: $env:ANDROID_PLATFORM_JAR"
+}
+
+$env:BOOTCLASSPATH = $env:JDK_HOME + "jre\lib"
+$env:CLASSPATH = "$env:UNITY_LIBS;$env:ANDROID_PLATFORM_JAR"
+
+$unityProjectRootPath = $buildConfig.UnityProjectRootPath
 $javaTempCompileRootPath = "$unityProjectRootPath\Temp\DeeplinkCompile\"
 CreateDirectoryIfNeeded -Path $javaTempCompileRootPath -m "Creating compile root directory"
 
@@ -45,7 +71,7 @@ $javaTemplateFile = Get-ChildItem -Path $javaTempCompileRootPath*.java.template 
 
 # insert company name from build settings and save to non-template java file
 Write-Output "Filling out template variables..."
-(Get-Content $javaTemplateFile).Replace("{packageName}", $jsonObject.AndroidPackageName) | Set-Content (($javaTemplateFile) -replace ".template$","") -Force 
+(Get-Content $javaTemplateFile).Replace("{packageName}", $buildConfig.AndroidPackageName) | Set-Content (($javaTemplateFile) -replace ".template$","") -Force 
 
 # Create a directory to do our compiling in
 $javaTempCompilePath = "$javaTempCompileRootPath\bin\"
@@ -59,8 +85,12 @@ Write-Output "Compiling ..."
 Write-Output "Manifest-Version: 1.0" > MANIFEST.MF
 Write-Output "Creating jar file..."
 $jarPath = "$javaTempCompileRootPath\UnityDeeplinks.jar"
+# slash means "here"
 & $env:JDK_HOME'\bin\jar' cvfM $jarPath -C $javaTempCompilePath /
 Copy-Item -Path $jarPath -Destination UnityDeeplinks.jar
+Write-Output ".jar created @ UnityDeeplinks.jar"
 
 # Clean up
+Write-Output "Cleaning up temorary files..."
 Remove-Item $javaTempCompileRootPath -Force  -Recurse -ErrorAction SilentlyContinue
+Write-Output "Done!"
